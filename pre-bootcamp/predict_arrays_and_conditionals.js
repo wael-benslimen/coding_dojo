dbc487be62ce4e953656e0c3622cf83bb9aa91e3
wael-benslimen <waelbenslimen1111@gmail.com>
2024-12-10 14:41:11 +0100

    Create predict_arrays_and_conditionals.js

---
+++ pre-bootcamp/predict_arrays_and_conditionals.js
@@ -0,0 +1,21 @@
+function displayContactInfo() {
+    var auntContactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+// resault : ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345]
+
+function displayGrocerylist() {
+    var produce = ["apples", "oranges"];
+    var frozen = ["broccoli", "ice cream"];
+    frozen.push("hashbrowns");
+    console.log(frozen);
+}
+
+//resault: ["broccoli", "ice cream","hashbrowns"]
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+//resault =  ["Bambi", "Beetlejuice", "Toy Story"]

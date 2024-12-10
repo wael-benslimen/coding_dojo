7922ebeb3d94dc6d9141121f18faa3cb9f370314
wael-benslimen <waelbenslimen1111@gmail.com>
2024-12-10 15:56:57 +0100

    Update return.js

--- programing basics/week1/day3/practice/return.js
+++ programing basics/week1/day3/practice/return.js
@@ -166,5 +166,35 @@ T_diagram
 sum = 5, sum = 8
 resault = 12
 
-the console 
+the console log is 
+sum is 5
+sum is 8
+resault is 12
+*/
+
+//10
+
+function sumNums(num1, num2) {
+    var sum = num1 + num2;
+    console.log('sum is', sum);
+    return sum;
+}
+var result = sumNums(2,3) + sumNums(3,sumNums(2,1)) + sumNums(sumNums(2,1),sumNums(2,3));
+console.log('result is', result);
+
+
+/*
+T_diagram
+sum = 5, sum = 3, sum = 6, sum = 3, sum = 5, sum = 8
+resault = 19
+
+consol log is 
+
+sum is 5
+sum is 3
+sum is 6
+sum is 3
+sum is 5
+sum is 8
+result is 19
 */

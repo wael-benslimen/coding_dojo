bc5336b12837aa02d4ecf50059b497a9929bbaf6
wael-benslimen <waelbenslimen1111@gmail.com>
2024-12-10 09:12:14 +0100

    Create predict_arrays.js

---
+++ programing basics/week1/day2/core/predict_arrays.js
@@ -0,0 +1,16 @@
+var contactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+console.log(contactInfo);
+// resault is:  ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345]
+
+var produce = ["apples", "oranges"];
+var frozen = ["broccoli", "ice cream"];
+frozen.push("hashbrowns");
+console.log(frozen);
+// resault is : ["broccoli", "ice cream","hashbrowns"]
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zorro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+// resault is : ["Bambi", "Beetlejuice", "Toy Story","Zorro"]

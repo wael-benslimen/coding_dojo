780dfbb611acd5085b4823b402766346851fd1a0
wael-benslimen <waelbenslimen1111@gmail.com>
2024-12-10 14:53:43 +0100

    Create code.js

---
+++ programing basics/week1/day2/optional/code.js
@@ -0,0 +1,7 @@
+//what would this variable do? store the number 0
+var likeCount = 0;
+//what would be the function of this code block?  increaselikes()
+function increaseLikes() {
+    // what is this sort operation for? a sum
+    likeCount = likeCount + 1;
+}

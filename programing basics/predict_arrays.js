e8ffbc6963c85066df356fea1e879f1c7e77c0dd
wael-benslimen <waelbenslimen1111@gmail.com>
2024-12-10 09:27:10 +0100

    adds

--- programing basics/predict_arrays.js
+++
@@ -1,2 +0,0 @@
-var contactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
-console.log(contactInfo);
---
+++ programing basics/week1/day1/core/get_on_that_ride.js
@@ -0,0 +1,19 @@
+// first streatch
+const height = prompt("enter your height ")
+const age  = prompt("enter your age ")
+if (height < 42  || age < 10) {
+    console.log("Sorry kiddo. Maybe next year.")
+}
+else{
+    console.log("Get on that ride, kiddo!")
+}
+
+// 2nd streatch
+const height = prompt("enter your height ")
+const age  = prompt("enter your age ")
+if (height < 42  && age < 10) {
+    console.log("Sorry kiddo. Maybe next year.")
+}
+else{
+    console.log("Get on that ride, kiddo!")
+}

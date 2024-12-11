98fe0a9b754f9455a08951fe010b0e0c41a9f2c6
wael-benslimen <waelbenslimen1111@gmail.com>
2024-12-11 08:04:28 +0100

    Create parameters.js

---
+++ programing basics/week1/day3/practice/parameters.js
@@ -0,0 +1,12 @@
+function greet(name, time) {
+
+if (name == "count dooku") {
+    console.log("I'm coming for you, Dooku!")
+    }
+else {
+    console.log("good day "+name+" it s "+time)
+    }
+    
+}
+
+greet("count dooku", 5)

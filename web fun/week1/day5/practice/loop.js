d525f2bfd2fb42e946272c2c4dea30ca62ae79fd
wael-benslimen <waelbenslimen1111@gmail.com>
2024-12-23 15:20:43 +0100

    s

---
+++ web fun/week1/day5/practice/loop.js
@@ -0,0 +1,28 @@
+for (let i = 1; i < 20; i+=2){
+    console.log(i); 
+}
+console.log("***************************");
+
+for (let i = 100; i > 0; i -= 1){
+    if (i % 3 == 0) {
+        console.log(i);
+        
+    }
+}
+console.log("****************************");
+
+for (let i = 4; i >= -3.5; i -= 1.5){
+    console.log(i);
+    
+}
+console.log("****************************");
+var sum = 0
+for (let i = 0; i <= 100; i++){
+    sum = sum + i
+}
+console.log(sum);
+let fact = 1
+for (let i = 1; i <= 12; i++){
+    fact = fact * i
+}
+console.log(fact);

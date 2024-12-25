d08c28749f9473d1a3a18992338a675cc8e871c5
wael-benslimen <waelbenslimen1111@gmail.com>
2024-12-25 08:59:16 +0100

    s

---
+++ web fun/week2/day2/Dojoweather/script.js
@@ -0,0 +1,16 @@
+function msg(){
+    alert("Loading weather report...")
+}
+function del(){
+    document.querySelector(".cookies").remove()
+}
+function tempchange(element){
+        document.querySelector("#hight1").innerHTML = "24°"
+        document.querySelector("#hight2").innerHTML = "27°"
+        document.querySelector("#hight3").innerHTML = "21°"
+        document.querySelector("#hight4").innerHTML = "26°"
+        document.querySelector("#low1").innerHTML = "18°"
+        document.querySelector("#low2").innerHTML = "19°"
+        document.querySelector("#low3").innerHTML = "16°"
+        document.querySelector("#low4").innerHTML = "21°"
+}

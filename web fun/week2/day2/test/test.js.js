24c2085e6283ea58d532429e95bd3a03a6a251fa
wael-benslimen <waelbenslimen1111@gmail.com>
2024-12-25 08:14:17 +0100

    c

--- web fun/week2/day2/test/test.js.js
+++
@@ -1,9 +0,0 @@
-function nameChange(){
-    newname = document.createElement("INPUT")
-    newname.setAttribute("type", "text");
-    newname.setAttribute("value", "write your name");
-    newname.setAttribute("class", "new-name")
-    newname.setAttribute("onchange", "backgroundChange()")
-    document.querySelector("#test").appendChild
-    (newname);
-}

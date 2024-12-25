7bdaa156deb9b6e199e70a2293d58f28f510eef4
wael-benslimen <waelbenslimen1111@gmail.com>
2024-12-25 17:50:32 +0100

    changes

--- web fun/week2/day2/Dojoweather/script.js
+++ web fun/week2/day2/Dojoweather/script.js
@@ -1,16 +1,23 @@
-function msg(){
+function msg() {
     alert("Loading weather report...")
 }
-function del(){
+function del() {
     document.querySelector(".cookies").remove()
 }
-function tempchange(element){
-        document.querySelector("#hight1").innerHTML = "24°"
-        document.querySelector("#hight2").innerHTML = "27°"
-        document.querySelector("#hight3").innerHTML = "21°"
-        document.querySelector("#hight4").innerHTML = "26°"
-        document.querySelector("#low1").innerHTML = "18°"
-        document.querySelector("#low2").innerHTML = "19°"
-        document.querySelector("#low3").innerHTML = "16°"
-        document.querySelector("#low4").innerHTML = "21°"
+function tempchange(element) {
+    const temunit = element.value;
+
+    const alltemps = document.querySelectorAll('.sidebyside p');
+
+    alltemps.forEach(tempElement => {
+        let temp = parseFloat(tempElement.textContent.replace('°', ''));
+
+        if (temunit === '°C') {
+            temp = (temp - 32) * (5 / 9);
+        } else {
+            temp = (temp * 9 / 5) + 32;
+        }
+
+        tempElement.textContent = temp.toFixed(0) + temunit;
+    });
 }

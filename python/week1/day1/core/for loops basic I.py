00a53cc656f5301a92c3b535249360907c229115
wael-benslimen <waelbenslimen1111@gmail.com>
2025-01-06 12:38:49 +0100

    Create for loops basic I.py

---
+++ python/week1/day1/core/for loops basic I.py
@@ -0,0 +1,25 @@
+for i in range (150):
+    print(i)
+
+for i in range (0, 1000, 5):
+    print(i)
+
+for i in range (1, 100):
+    if (i % 10 == 0):
+        print("coding dojo")
+    elif(i % 5 == 0):
+        print("coding")
+x = 0
+for i in range (500000):
+    x += i
+print(x)
+
+for i in range(2018, 0, -4):
+    print(i)
+
+lownum = 0
+hightnum = 50
+mult = 5
+for i in range (lownum, hightnum):
+    if(i % mult == 0):
+        print(i)

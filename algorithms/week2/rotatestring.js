589b4783858859e4cac56b1b1a516f395d929149
wael-benslimen <waelbenslimen1111@gmail.com>
2025-01-15 13:29:49 +0100

    Update rotatestring.js

--- algorithms/week2/rotatestring.js
+++ algorithms/week2/rotatestring.js
@@ -1,9 +1,9 @@
 /*
 steps/
 1/ we creat machine that takes a data from the client that is sentence and a number
-2/ we cheack if the number given is 0 or in the same value as the lenght of sentence given  return the same sentence
-3/ we cheack if the number given is highter then the lenght of the sentence 
-4/ we substract lenght of the sentence from the number given
+2/ we cheack if the number given is 0 or in the same value as the length of sentence given  return the same sentence
+3/ we cheack if the number given is highter then the length of the sentence 
+4/ we substract length of the sentence from the number given
 5/ we creat an empty container 
 6/ we loop throgh the sentence  starting from the end and stops at the number given
 7/ we add the carecters from the end of the sentence to the previosly created container and remove them 1 by one
@@ -24,24 +24,25 @@ Tdiagrame
 
 
 function retateStr(str, amnt) {
-    str2 = ""
-    if (amnt == 0 || amnt == str.lenght - 1) {
+    let str2 = ""
+    if (amnt == 0 || amnt == str.length) {
         console.log("if 1");
         return str
     }
-    if (amnt > str.lenght) {
-        amnt = amnt - str.lenght
-        console.log("if 2", amnt);
+    if (amnt > str.length) {
+        amnt = amnt - str.length
 
     }
-
-    for (let i = str.length - 1; i > amnt; i--) {
-        str2 = str2 + str[i]
+    for (let i = str.length - 1; i > str.length-amnt-1 ; i--){
+        str2 = str[i] + str2 
         console.log(str2);
-        str
     }
+    console.log(str2);
+    console.log(str2 + str.replace(str2, ""));
+    
+    
 }
 
 
 
-console.log(retateStr("Hello world", 2));
+console.log(retateStr("Hello world", 11));

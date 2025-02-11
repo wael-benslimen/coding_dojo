c226114043a134fa2022d63701e8173276b6a73e
wael-benslimen <waelbenslimen1111@gmail.com>
2025-02-11 18:55:51 +0100

    first jsx

---
+++ MERN/day2/core/firstReactWithJSX/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>hello Dojo</h1>
+      <h2>Things i need to do :</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>climb Mt. everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </>
+  )
+}
+
+export default App

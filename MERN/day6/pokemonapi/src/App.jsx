5283529dfb5656bfa947e4dcf074e8b433ee3de1
wael ben slimen <waelbenslimen1111@gmail.com>
2025-02-18 14:28:19 +0100

    pokemon

---
+++ MERN/day6/pokemonapi/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+function App() {
+  const [pokemons, setPokemons] = useState([])
+
+  const fetchPokemons = () => {
+      axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=200')
+        .then(response => {
+        console.log("this is the response after .json",response)
+          const firstpokemons = response.data.results.slice(0, 151)
+          console.log(firstpokemons);
+          
+        setPokemons(firstpokemons)
+        
+    })
+  }
+
+  useEffect(() => {
+    fetchPokemons()
+  },[])
+
+  
+
+  return (
+    <div className=' container mx-auto mt-20 min-h-dvh flex flex-col p-1 items-center bg-amber-100 rounded-2xl border gap-5'>
+      <h1 className=' text-3xl font-extrabold text-blue-300'>Pokemon API</h1>
+      <ul className=' list-decimal flex flex-col'>
+        {pokemons.map((p, index) => 
+          <li key={index} className=' list-item font-bold '> {p.name} </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App

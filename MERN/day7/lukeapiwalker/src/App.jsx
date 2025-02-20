c438df58136c65fb6eb3461591eef3cf100e92c1
wael ben slimen <waelbenslimen1111@gmail.com>
2025-02-20 13:55:28 +0100

    Connecting with Mongoose

--- MERN/day7/lukeapiwalker/src/App.jsx
+++ MERN/day7/lukeapiwalker/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import axios from 'axios'
 import DisplayData from "./components/DisplayData"
 function App() {
--- algorithms/quickSort.js
+++ algorithms/quickSort.js
@@ -1,14 +1,24 @@
-const partition=(arr) => {
-    let pivot = Math.floor(arr.length/2)
-    let left = arr[0]
-    let right = arr[arr.length - 1]
-    console.log('this is left',left);
-    console.log('this is right', right);
-    for (let i = 0; i < pivot; i++){
-         
+const partition = (arr) => {
+    let pivot = arr[Math.floor(arr.length / 2)];
+    let left = [];
+    let right = [pivot];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else if (arr[i] > pivot) {
+            right.push(arr[i]);
+        }
     }
-    
-    return arr
-}
 
-console.log(partition([4, 3, 9, 1, 5, 6, 0 ]));
+    arr = [...left, ...right];
+    let index = arr.findIndex((item)=>item == pivot);
+    return index;
+};
+console.log('after partition', partition([4, 3, 10, 1, 5, 6, 0]));
+
+
+const quickSort = (arr) => {
+    let left = []
+    let right = []
+
+}

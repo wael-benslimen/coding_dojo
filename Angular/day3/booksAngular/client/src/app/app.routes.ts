e7e86ae7a066aeec12209be214fa8e86feb03d57
wael ben slimen <waelbenslimen1111@gmail.com>
2025-03-10 10:54:53 +0100

    User model

--- Angular/day3/booksAngular/client/src/app/app.routes.ts
+++ Angular/day3/booksAngular/client/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { ViewAllComponent } from '../components/view-all/view-all.component';
 import { CreateOneComponent } from '../components/create-one/create-one.component';
+import { EditComponent } from '../components/edit/edit.component';
 
 export const routes: Routes = [
   { path: '', component: ViewAllComponent },
   { path: 'create', component: CreateOneComponent },
+  { path: 'edit/:id', component: EditComponent },
+
 ];
--- Angular/day3/booksAngular/client/src/components/view-all/view-all.component.ts
+++ Angular/day3/booksAngular/client/src/components/view-all/view-all.component.ts
@@ -2,20 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Book } from '../../interfaces/book';
 import { ApiService } from '../../api.service';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-view-all',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './view-all.component.html',
   styleUrl: './view-all.component.css',
 })
 export class ViewAllComponent implements OnInit {
   allBooks: Book[] = [];
-  constructor(private apiservice: ApiService) { }
+  constructor(private apiservice: ApiService) {}
   ngOnInit(): void {
     this.apiservice.getall().subscribe({
-      next: data => this.allBooks = data,
-      error: err => console.error(err)
-    })
+      next: (data) => (this.allBooks = data),
+      error: (err) => console.error(err),
+    });
   }
 }

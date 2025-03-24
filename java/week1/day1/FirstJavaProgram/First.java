348f260557cd56b03753bd19180dc9aae17be7e4
wael ben slimen <waelbenslimen1111@gmail.com>
2025-03-24 13:49:11 +0100

    first java

---
+++ java/week1/day1/FirstJavaProgram/First.java
@@ -0,0 +1,8 @@
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("My name is wael");
+        System.out.println("I'm 22 years old");
+        System.out.println("My home town is Ghar Melh");
+    }
+}

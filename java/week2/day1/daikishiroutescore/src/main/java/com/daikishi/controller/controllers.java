028803264ea5ed4c63519d446f9ca1032efab853
wael ben slimen <waelbenslimen1111@gmail.com>
2025-04-02 18:13:10 +0100

    core

---
+++ java/week2/day1/daikishiroutescore/src/main/java/com/daikishi/controller/controllers.java
@@ -0,0 +1,35 @@
+package com.daikishi.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controllers {
+	@RequestMapping("/daikichi")
+	public String greeting() {
+		return "Welcome";
+	}
+	
+	@RequestMapping("/daikichi/today")
+	public String today() {
+		return "Today you will find luck";
+	}
+	
+	@RequestMapping("/daikichi/tomorrow")
+	public String tomorrow() {
+		return "tomorrow an oportunitywill arise so be sure tobe open to new ideas";
+	}
+
+	@RequestMapping("/daikichi/travel/{location}")
+	public String travel(@PathVariable("location") String location){
+		return "congrats you will travel to "+ location;
+	}
+	
+	@RequestMapping("/daikichi/lotto/{age}")
+	public String lotto(@PathVariable("age") int age) {
+		String expresion = age < 30 ?  "you will take a good jorny..." :  "go have fun with your family ...";
+		return expresion;
+	}
+	
+}
